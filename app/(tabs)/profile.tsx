c861d87b69da612335d0d3461792d6e6d853a7c4
Kingsley Eneja <kingsley.eneja@getir.com>
2025-05-09 19:08:07 -0300

    Initial commit

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Profile = () => {
+  return (
+    <View className="flex-1 justify-center items-center border-2 border-green-500">
+      <Text>Profile</Text>
+    </View>
+  );
+};
+
+export default Profile;
---
+++ app/(tabs)/saved.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Saved = () => {
+  return (
+    <View className="flex-1 justify-center items-center border-2 border-blue-500" >
+      <Text>Saved</Text>
+    </View>
+  );
+};
+
+export default Saved;
